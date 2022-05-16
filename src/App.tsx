3d6b8222f43070de5d39540c24b13dcc07205d87
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-16 13:33:20 +0100

    import signupform

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { SignUpForm } from "./Components/SignUpForm/SignUpForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Sign in</p>
+        <p>Create Account</p>
+        <p>Test Account</p>
+      </header>
+      <SignUpForm />
+    </div>
+  );
 }
 
 export default App;
