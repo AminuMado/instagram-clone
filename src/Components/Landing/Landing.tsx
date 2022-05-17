a86cf06c8c1376dd2ade6aa81a7808025206900e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-17 23:28:12 +0100

    finished version 1

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -7,18 +7,33 @@ import { useState } from "react";
 
 export const Landing = () => {
   const [showOverlay, setShowOverlay] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+  const handleSignInClick = () => {
+    setShowOverlay(true);
+    setShowLogin(true);
+  };
+  const handleCreateAccountClick = () => {
+    setShowOverlay(true);
+    setShowSignUpForm(true);
+  };
+  const clearAll = () => {
+    setShowOverlay(false);
+    setShowLogin(false);
+    setShowSignUpForm(false);
+  };
   return (
     <div className="Landing">
       <header className="Landing-header">
-        <p onClick={(e) => setShowOverlay(true)}>Sign in</p>
-        <p>Create Account</p>
+        <p onClick={handleSignInClick}>Sign in</p>
+        <p onClick={handleCreateAccountClick}>Create Account</p>
         <Link to="/Home">
           <p>Test Account</p>
         </Link>
       </header>
-      <SignUpForm />
-      <Login />
-      <Overlay showOverlay={showOverlay} />
+      <SignUpForm active={showSignUpForm} />
+      <Login active={showLogin} />
+      <Overlay active={showOverlay} handleClick={clearAll} />
     </div>
   );
 };
