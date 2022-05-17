e2674c9c3576fe596482338488d69430da369cb8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-17 23:27:33 +0100

    add props and styling

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,33 +1,39 @@
 import "./Login.css";
 import { useState } from "react";
-export const Login = () => {
+
+type LoginProps = {
+  active: boolean;
+};
+export const Login = (props: LoginProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <form className="loginForm">
-      <div className="wrapper_container">
-        <div>
-          <label htmlFor="email"> Email</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            id="email"
-            type="email"
-            placeholder="bobbafett@example.com"
-            value={email}
-          ></input>
+    <div className={props.active ? "active" : "loginForm_container"}>
+      <form className="loginForm">
+        <div className="wrapper_container">
+          <div>
+            <label htmlFor="email"> Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              type="email"
+              placeholder="bobbafett@example.com"
+              value={email}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="password"> Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              type="password"
+              value={password}
+            ></input>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password"> Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            id="password"
-            type="password"
-            value={password}
-          ></input>
-        </div>
-      </div>
 
-      <button onClick={(e) => e.preventDefault()}>Sign in</button>
-    </form>
+        <button onClick={(e) => e.preventDefault()}>Sign in</button>
+      </form>
+    </div>
   );
 };
