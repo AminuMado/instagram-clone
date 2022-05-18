542f139d32b89e748984c63c5c540345ac720cd9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-18 22:54:13 +0100

    add background image

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,4 +1,14 @@
+import { Post } from "../Post/Post";
 import "./Home.css";
+
 export const Home = () => {
-  return <div>Hello Home</div>;
+  return (
+    <div className="Home">
+      Hello Home
+      <Post />
+      {/* Header / Nav Component */}
+      {/* Posts */}
+      {/* Sidebar for Helper functions or for profile showing */}
+    </div>
+  );
 };
