a0e149aa852bde9cf1f9b5ab638e19117010767d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-18 23:07:01 +0100

    initial template

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,5 +1,18 @@
 import "./Post.css";
-
+import img_Src from "../../Assets/Images/wallpaper5.jpg";
+import { Avatar } from "../Avatar/Avatar";
+import avatar from "../../Assets/Images/Avatars/7.png";
 export const Post = () => {
-  return <div>Hello Post</div>;
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar src={avatar} handleClick={() => {}} />
+        <h3>UserName</h3>
+      </div>
+      <img className="post__image" src={img_Src} alt="post" />
+      <h4 className="post__text">
+        <strong>The Man Himself:</strong>First Post
+      </h4>
+    </div>
+  );
 };
