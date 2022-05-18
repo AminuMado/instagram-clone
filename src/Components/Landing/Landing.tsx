19616a39d0427ee51abc6e0fafa8da249de13400
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-18 11:40:36 +0100

    add setState props

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -4,11 +4,13 @@ import { SignUpForm } from "../SignUpForm/SignUpForm";
 import { Link } from "react-router-dom";
 import Overlay from "../Overlay/Overlay";
 import { useState } from "react";
+import { Avatar } from "../Avatar/Avatar";
 
 export const Landing = () => {
   const [showOverlay, setShowOverlay] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [showSignUpForm, setShowSignUpForm] = useState(false);
+  const [showAvatar, setShowAvatar] = useState(false);
   const handleSignInClick = () => {
     setShowOverlay(true);
     setShowLogin(true);
@@ -21,6 +23,7 @@ export const Landing = () => {
     setShowOverlay(false);
     setShowLogin(false);
     setShowSignUpForm(false);
+    setShowAvatar(false);
   };
   return (
     <div className="Landing">
@@ -31,7 +34,8 @@ export const Landing = () => {
           <p>Test Account</p>
         </Link>
       </header>
-      <SignUpForm active={showSignUpForm} />
+      <Avatar active={showAvatar} setShowAvatar={setShowAvatar} />
+      <SignUpForm active={showSignUpForm} setShowAvatar={setShowAvatar} />
       <Login active={showLogin} />
       <Overlay active={showOverlay} handleClick={clearAll} />
     </div>
