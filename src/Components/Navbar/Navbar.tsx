74909a9fc8bd9cd2766d6ce35d7b033e9feb5e6b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-19 22:12:18 +0100

    import settings icon

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,4 +1,12 @@
 import "./Navbar.css";
+import settings1_Src from "../../Assets/Images/settings1.png";
+import settings2_Src from "../../Assets/Images/settings2.png";
 export const Navbar = () => {
-  return <div>Hello Navbar</div>;
+  return (
+    <div className="navbar">
+      <h1>Star Wars World</h1>
+      {/* <img src={settings1_Src}></img> */}
+      <img src={settings2_Src}></img>
+    </div>
+  );
 };
