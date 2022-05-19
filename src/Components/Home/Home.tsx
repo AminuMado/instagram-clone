a724f04bb5c8e8bc5d1348cbfed0b652c20332d1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-19 22:11:23 +0100

    add navbar component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,14 +1,12 @@
+import { Navbar } from "../Navbar/Navbar";
 import { Post } from "../Post/Post";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="Home">
-      Hello Home
+      <Navbar />
       <Post />
-      {/* Header / Nav Component */}
-      {/* Posts */}
-      {/* Sidebar for Helper functions or for profile showing */}
     </div>
   );
 };
