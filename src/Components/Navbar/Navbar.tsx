d2bb0fea3b64f9fe506706cb3fcd48d1aee8dfc3
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-20 23:11:49 +0100

    add light and dark mode icon

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,8 +2,10 @@ import "./Navbar.css";
 import settings_Src from "../../Assets/Images/settings.png";
 import back_Src from "../../Assets/Images/back1.png";
 import { useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 export const Navbar = () => {
   const [showOptions, setShowOptions] = useState(false);
+  const [mode, setMode] = useState(false);
   return (
     <nav className="navbar">
       <h1>Star Wars World</h1>
@@ -23,6 +25,12 @@ export const Navbar = () => {
           src={back_Src}
           alt="back_icon"
         ></img>
+        <DarkModeSwitch
+          onChange={() => setMode(!mode)}
+          checked={mode}
+          size={40}
+          className="dark_mode_switch"
+        />
       </nav>
     </nav>
   );
