b058171c461135292f178858b9725f552eb55866
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-22 20:05:12 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Landing } from "./Components/Landing/Landing";
 import { Home } from "./Components/Home/Home";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Profile } from "./Components/Profile/Profile";
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
