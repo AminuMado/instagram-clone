cd5486bd4524d1f21edf62b4e9669909e5ca342e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-23 10:20:46 +0100

    add route to postModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Landing } from "./Components/Landing/Landing";
 import { Home } from "./Components/Home/Home";
-
+import { PostModal } from "./Components/Post/PostModal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./Components/Profile/Profile";
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="Post/:id" element={<PostModal />} />
       </Routes>
     </BrowserRouter>
   );
