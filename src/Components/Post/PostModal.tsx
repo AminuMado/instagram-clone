c046bde6f00cf823a1fcff3e1d492138c5b5e1a6
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-25 11:36:02 +0100

    add container and formating for objects

--- src/Components/Post/PostModal.tsx
+++ src/Components/Post/PostModal.tsx
@@ -8,79 +8,85 @@ export const PostModal = () => {
   const [isLiked, setIsLiked] = useState(false);
   return (
     <div className="postModal">
-      <div className="postModal__header">
-        <div className="postModal__header_left">
-          <Avatar src={avatar} handleClick={() => {}} />
-          <h3>The Man Himself</h3>
+      <div className="postModal__container">
+        <div className="postModal__image_container">
+          <img className="postModal__image" src={img_Src} alt="post" />
         </div>
-        <div className="postModal__header_right">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            stroke="currentColor"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="three_dots_icon"
-          >
-            <circle cx="12" cy="12" r="1"></circle>
-            <circle cx="19" cy="12" r="1"></circle>
-            <circle cx="5" cy="12" r="1"></circle>
-          </svg>
+        <div className="postModal__details_container">
+          <div className="postModal__header">
+            <div className="postModal__header_left">
+              <Avatar src={avatar} handleClick={() => {}} />
+              <h3>The Man Himself</h3>
+            </div>
+            <div className="postModal__header_right">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="three_dots_icon"
+              >
+                <circle cx="12" cy="12" r="1"></circle>
+                <circle cx="19" cy="12" r="1"></circle>
+                <circle cx="5" cy="12" r="1"></circle>
+              </svg>
+            </div>
+          </div>
+          <div className="postModal__icons">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill={isLiked ? "red" : "none"}
+              viewBox="0 0 24 24"
+              stroke={isLiked ? "red" : "currentcolor"}
+              className="like_icon"
+              onClick={() => setIsLiked(!isLiked)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="comment_icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+              ></path>
+            </svg>
+            <span className="num_of_likes">1 like</span>
+          </div>
+
+          <h4 className="postModal__text">
+            <strong>The Man Himself:</strong>First Post
+          </h4>
+
+          <form className="postModal__comment">
+            <input
+              aria-label="Add a comment"
+              className="post__comment_input"
+              type="text"
+              name="add-comment"
+              placeholder="Add a comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button className="postModal__comment_button" type="button">
+              Post
+            </button>
+          </form>
         </div>
       </div>
-      <img className="postModal__image" src={img_Src} alt="post" />
-      <div className="postModal__icons">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill={isLiked ? "red" : "none"}
-          viewBox="0 0 24 24"
-          stroke={isLiked ? "red" : "currentcolor"}
-          className="like_icon"
-          onClick={() => setIsLiked(!isLiked)}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-          ></path>
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="comment_icon"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-          ></path>
-        </svg>
-        <span className="num_of_likes">1 like</span>
-      </div>
-
-      <h4 className="postModal__text">
-        <strong>The Man Himself:</strong>First Post
-      </h4>
-
-      <form className="postModal__comment">
-        <input
-          aria-label="Add a comment"
-          className="post__comment_input"
-          type="text"
-          name="add-comment"
-          placeholder="Add a comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <button className="postModal__comment_button" type="button">
-          Post
-        </button>
-      </form>
     </div>
   );
 };
