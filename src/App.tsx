a5f7d6a153cb50e4beeb4d2c5643b289fa583559
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-01 22:55:28 +0100

    add themeContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { Home } from "./Components/Home/Home";
 import { PostModal } from "./Components/Post/PostModal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./Components/Profile/Profile";
+import { ThemeContextProvider } from "./Context/ThemeContext";
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="Post/:id" element={<PostModal />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContextProvider>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="Post/:id" element={<PostModal />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContextProvider>
   );
 }
 
