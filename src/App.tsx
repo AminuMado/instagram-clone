25164bdc5a271dad925beb3a9455034716d0b4af
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-04 23:44:33 +0100

    add userContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import { PostModal } from "./Components/Post/PostModal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./Components/Profile/Profile";
 import { ThemeContextProvider } from "./Context/ThemeContext";
+import { UserContextProvider } from "./Context/UserContext";
 function App() {
   return (
     <ThemeContextProvider>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="Post/:id" element={<PostModal />} />
-        </Routes>
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="Post/:id" element={<PostModal />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContextProvider>
     </ThemeContextProvider>
   );
 }
