80b83cfbec7feb58326af93b3b6132f443d810c9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-05 16:20:55 +0100

    add upload file functionality

--- src/Components/Post/NewPost.tsx
+++ src/Components/Post/NewPost.tsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
 import "./NewPost.css";
+import { storage } from "../../Utils/firebase";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  getMetadata,
+} from "firebase/storage";
 
 type ImageUploadProps = {
   active: boolean;
 };
 export const ImageUpload = (props: ImageUploadProps) => {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState<File | null>(null);
   const [url, setUrl] = useState("");
   const [progress, setProgress] = useState(0);
   const [caption, setCaption] = useState("");
 
   const handleChange = (e: any) => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
     }
   };
   const handleUpload = () => {
-    // // const uploadTask  = storage.ref(`images${image.name}`).put(image);
+    // we are tryping to upload a file to the storage
+    // get the file's url  we just stored
+    // use that url to save the file on our db
+    // add a user id and username to the post uploaded
+    // Upload
+    if (image) {
+      const imageRef = ref(storage, `images/${image.name}`);
+      uploadBytesResumable(imageRef, image)
+        .then((snapshot) => {
+          console.log("Uploaded", snapshot.totalBytes, "bytes.");
+          console.log("File metadata:", snapshot.metadata);
+          // Let's get a download URL for the file.
+          getDownloadURL(snapshot.ref).then((url) => {
+            console.log("File available at", url);
+            // ...
+          });
+        })
+        .catch((error) => {
+          console.error("Upload failed", error);
+          // ...
+        });
+    }
+
+    // const uploadTask = storage.ref(`images${image.name}`).put(image);
     // uploadTask.on(
     //   "state_changed",
     //   (snapshot) => {
