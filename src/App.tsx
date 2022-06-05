644cac4d3055175fb3e85b45086e88bbd487a535
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-05 01:02:13 +0100

    add conditional render if user is signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
 import { Landing } from "./Components/Landing/Landing";
 import { Home } from "./Components/Home/Home";
@@ -6,20 +6,19 @@ import { PostModal } from "./Components/Post/PostModal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./Components/Profile/Profile";
 import { ThemeContextProvider } from "./Context/ThemeContext";
-import { UserContextProvider } from "./Context/UserContext";
+import { UserContext } from "./Context/UserContext";
 function App() {
+  const { user } = useContext(UserContext);
   return (
     <ThemeContextProvider>
-      <UserContextProvider>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Profile" element={<Profile />} />
-            <Route path="Post/:id" element={<PostModal />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContextProvider>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={user ? <Home /> : <Landing />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="Post/:id" element={<PostModal />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeContextProvider>
   );
 }
