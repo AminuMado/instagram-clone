f303fe6193bff8d790024b5a57aae27752c7bb0e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-06 04:30:22 +0100

    update user object type to User

--- src/Context/UserContext.tsx
+++ src/Context/UserContext.tsx
@@ -1,10 +1,11 @@
+import { User } from "firebase/auth";
 import React, { createContext, useState } from "react";
 type UserContextProviderProps = {
   children: React.ReactNode;
 };
 type UserContextType = {
-  user: {} | null;
-  setUser: React.Dispatch<React.SetStateAction<{} | null>>;
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
 };
 
 export const UserContext = createContext({} as UserContextType);
@@ -13,7 +14,7 @@ export const UserContextProvider = (props: UserContextProviderProps) => {
   /* Local Storage */
   // we check local storage for an existing user if you exist we set you to the user saved else we set user to null
   const getUser = (() => {
-    let currentUser: string | null = null;
+    let currentUser: User | null = null;
     const value = localStorage.getItem("currentUser");
     if (typeof value === "string") {
       currentUser = JSON.parse(value);
@@ -21,7 +22,7 @@ export const UserContextProvider = (props: UserContextProviderProps) => {
     return currentUser;
   })();
 
-  const [user, setUser] = useState<{} | null>(getUser);
+  const [user, setUser] = useState<User | null>(getUser);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
