8c6e16181dcde604374df0766a6cd544253c24c6
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-06 23:53:33 +0100

    add post comment funtionality

--- src/Components/Comments/AddComment.tsx
+++ src/Components/Comments/AddComment.tsx
@@ -1,7 +1,32 @@
 import "./AddComment.css";
-import { useState } from "react";
-const AddComment = () => {
+import { useState, useContext } from "react";
+import { db } from "../../Utils/firebase";
+import { UserContext } from "../../Context/UserContext";
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+
+type AddCommentProps = {
+  postId: string;
+};
+
+export const AddComment = (props: AddCommentProps) => {
+  console.log(props.postId);
   const [comment, setComment] = useState("");
+  const { user } = useContext(UserContext);
+  const addComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (comment.trim() === "") return;
+    if (user) {
+      const docRef = doc(db, "posts", `${props.postId}`);
+      updateDoc(docRef, {
+        comments: arrayUnion({
+          id: user.uid,
+          text: comment,
+          username: user.displayName,
+        }),
+      });
+      setComment("");
+    }
+  };
   return (
     <form className="post__add_comment" onSubmit={addComment}>
       <input
