5ac1f68d0db1cd1d5fecfa5083153fcf25313e14
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-08 11:29:03 +0100

    add ActivePost Context

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./Components/Profile/Profile";
 import { ThemeContextProvider } from "./Context/ThemeContext";
 import { UserContext } from "./Context/UserContext";
+import { ActivePostContextProvider } from "./Context/ActivePostContext";
+import { ActivePostContext } from "./Context/ActivePostContext";
+
 function App() {
   const { user } = useContext(UserContext);
+  const { activePost } = useContext(ActivePostContext);
   return (
     <ThemeContextProvider>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={user ? <Home /> : <Landing />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="Post/:id" element={<PostModal />} />
-        </Routes>
-      </BrowserRouter>
+      <ActivePostContextProvider>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={user ? <Home /> : <Landing />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="Post/:id" element={<PostModal post={activePost} />} />
+          </Routes>
+        </BrowserRouter>
+      </ActivePostContextProvider>
     </ThemeContextProvider>
   );
 }
