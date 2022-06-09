711f164c937138df27884a9954b56dc298d20729
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-09 12:05:14 +0100

    add new handlesetactivepost funtion

--- src/Context/ActivePostContext.tsx
+++ src/Context/ActivePostContext.tsx
@@ -17,7 +17,7 @@ type comment = {
 };
 type ActivePostContextType = {
   activePost: post;
-  setActivePost: React.Dispatch<React.SetStateAction<post>>;
+  handleSetActivePost: (post: post) => void;
 };
 
 export const ActivePostContext = createContext({} as ActivePostContextType);
@@ -25,17 +25,31 @@ export const ActivePostContext = createContext({} as ActivePostContextType);
 export const ActivePostContextProvider = (
   props: ActivePostContextProviderProps
 ) => {
-  const [activePost, setActivePost] = useState<post>({
-    id: "",
-    caption: "",
-    imageUrl: "",
-    username: "",
-    avatar: "",
-    comments: [],
-  });
+  /* Local Storage */
+  // we check local storage for an existing activepost if you exist we set you to the activepost state else we set it to an empty template
+  const getActivePost = (() => {
+    let currentActivePost: post = {
+      id: "",
+      caption: "",
+      imageUrl: "",
+      username: "",
+      avatar: "",
+      comments: [],
+    };
+    const value = localStorage.getItem("currentActivePost");
+    if (typeof value === "string") {
+      currentActivePost = JSON.parse(value);
+    }
+    return currentActivePost;
+  })();
+  const [activePost, setActivePost] = useState<post>(getActivePost);
+  const handleSetActivePost = (post: post) => {
+    localStorage.setItem("currentActivePost", JSON.stringify(post)); // save the activePost in localStorage
+    setActivePost(post);
+  };
 
   return (
-    <ActivePostContext.Provider value={{ activePost, setActivePost }}>
+    <ActivePostContext.Provider value={{ activePost, handleSetActivePost }}>
       {props.children}
     </ActivePostContext.Provider>
   );
