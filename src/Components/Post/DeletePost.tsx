e9ba2421979b1e36e9c378f13fa2464d3436ac4a
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-10 12:22:22 +0100

    implement delete post

--- src/Components/Post/DeletePost.tsx
+++ src/Components/Post/DeletePost.tsx
@@ -4,26 +4,12 @@ import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../../Utils/firebase";
 
 type DeletePostProps = {
-  post: post;
+  postId: string;
 };
-type post = {
-  id: string;
-  caption: string;
-  imageUrl: string;
-  username: string;
-  avatar: string;
-  comments: comment[];
-  postBy: string;
-  likedBy: [];
-};
-type comment = {
-  text: string;
-  username: string;
-  id: string;
-};
+
 export const DeletePost = (props: DeletePostProps) => {
   const handleClick = async () => {
-    await deleteDoc(doc(db, "posts", props.post.id));
+    await deleteDoc(doc(db, "posts", props.postId));
   };
   return (
     <img
