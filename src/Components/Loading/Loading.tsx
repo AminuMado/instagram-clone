e2004f812cde641b11549c6e998d12e58043f76f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-10 20:17:35 +0100

    add is loading context

--- src/Components/Loading/Loading.tsx
+++ src/Components/Loading/Loading.tsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+import { LoadingContext } from "../../Context/LoadingContext";
 import "./Loading.css";
 
 export const Loading = () => {
+  const { isLoading } = useContext(LoadingContext);
   return (
-    <div className="loading_overlay active">
+    <div className={isLoading ? "loading_overlay active" : "loading_overlay"}>
       <div className="loading_wrapper">
         <div className="loading_animation"></div>
       </div>
