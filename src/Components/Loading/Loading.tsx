80303b9280b4c7e987325cdab86541e980ee0771
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-10 20:14:47 +0100

    clean up unused code

--- src/Components/Loading/Loading.tsx
+++ src/Components/Loading/Loading.tsx
@@ -2,8 +2,10 @@ import "./Loading.css";
 
 export const Loading = () => {
   return (
-    <div className="loading">
-      <div className="loadingAnimation"></div>
+    <div className="loading_overlay active">
+      <div className="loading_wrapper">
+        <div className="loading_animation"></div>
+      </div>
     </div>
   );
 };
