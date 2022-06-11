07e906eda00df771c8b1ad1210f96d6bb781d7fb
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-11 20:50:20 +0100

    add input fields for avatar and username

--- src/Components/Profile/EditProfile.tsx
+++ src/Components/Profile/EditProfile.tsx
@@ -1,26 +1,44 @@
 import { useState } from "react";
 import { AvatarPicker } from "../Avatar/AvatarPicker";
+import { Avatar } from "../Avatar/Avatar";
+
 import "./EditProfile.css";
 export const EditProfile = () => {
   const [username, setUsername] = useState("");
+  const [showAvatarPicker, setShowAvatarPicker] = useState(false);
+  const [avatar, setAvatar] = useState("");
   return (
-    <div className="editprofile_overlay">
-      <div className="editprofile_wrapper">
-        <div className="editprofile_input">
-          <label htmlFor="username">UserName</label>
-          <input
-            onChange={(e) => setUsername(e.target.value)}
-            id="username"
-            type="text"
-            placeholder="Bobba Fett"
-            value={username}
-          ></input>
+    <div className="editprofile">
+      <div className="editprofile_container">
+        <div className="editprofile_avatar_container">
+          <label htmlFor="avatar">Avatar</label>
+          <Avatar
+            src={avatar}
+            handleClick={() => {
+              setShowAvatarPicker(true);
+            }}
+          />
         </div>
-        {/* <AvatarPicker active={true} setShowAvatarPicker={false}/> */}
-        {/* avatarpicker for the avatar */}
-        {/* update button */}
-        {/* close modal or cancel button */}
+        <label htmlFor="username">UserName</label>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          id="username"
+          type="text"
+          placeholder="Bobba Fett"
+          value={username}
+        ></input>
+        <div className="editprofile_buttons">
+          <button>Update</button>
+        </div>
       </div>
+      <AvatarPicker
+        active={showAvatarPicker}
+        setShowAvatarPicker={setShowAvatarPicker}
+        setAvatar={setAvatar}
+      />{" "}
+      {/* avatarpicker for the avatar */}
+      {/* update button */}
+      {/* close modal or cancel button */}
     </div>
   );
 };
