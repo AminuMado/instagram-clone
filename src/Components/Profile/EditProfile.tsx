d8d18d1289b9557926354eada01a94c038f5729d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-11 21:21:23 +0100

    add component props

--- src/Components/Profile/EditProfile.tsx
+++ src/Components/Profile/EditProfile.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import { AvatarPicker } from "../Avatar/AvatarPicker";
 import { Avatar } from "../Avatar/Avatar";
-
 import "./EditProfile.css";
-export const EditProfile = () => {
+
+type EditProfileProps = {
+  active: boolean;
+  toggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export const EditProfile = (props: EditProfileProps) => {
   const [username, setUsername] = useState("");
   const [showAvatarPicker, setShowAvatarPicker] = useState(false);
   const [avatar, setAvatar] = useState("");
   return (
     <div className="editprofile">
+      <div className="editprofile_overlay"></div>
       <div className="editprofile_container">
         <div className="editprofile_avatar_container">
           <label htmlFor="avatar">Avatar</label>
@@ -30,15 +35,15 @@ export const EditProfile = () => {
         <div className="editprofile_buttons">
           <button>Update</button>
         </div>
+        <button onClick={() => props.toggle(false)} className="close_btn">
+          Close x
+        </button>
       </div>
       <AvatarPicker
         active={showAvatarPicker}
         setShowAvatarPicker={setShowAvatarPicker}
         setAvatar={setAvatar}
-      />{" "}
-      {/* avatarpicker for the avatar */}
-      {/* update button */}
-      {/* close modal or cancel button */}
+      />
     </div>
   );
 };
