0d80cac14bb8125caf32dcdccd879b74c014d1fa
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-14 13:40:35 +0100

    add colRef

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { collection, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,3 +27,4 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+export const colRef = collection(db, "posts");
