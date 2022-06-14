2fe581f82ca692436a005fc080738b1130e732d2
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-06-14 12:59:32 +0100

    add login details for test account

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -2,14 +2,23 @@ import "./Landing.css";
 import { Login } from "../Login/Login";
 import { SignUpForm } from "../SignUpForm/SignUpForm";
 import Overlay from "../Overlay/Overlay";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Loading } from "../Loading/Loading";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Utils/firebase";
+import { LoadingContext } from "../../Context/LoadingContext";
+import { UserContext } from "../../Context/UserContext";
 
+const testAccount = {
+  email: "startropperA@example.com",
+  password: "12345678",
+};
 export const Landing = () => {
   const [showOverlay, setShowOverlay] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [showSignUpForm, setShowSignUpForm] = useState(false);
-
+  const { handleSetIsLoading } = useContext(LoadingContext);
+  const { setUser } = useContext(UserContext);
   const handleSignInClick = () => {
     setShowOverlay(true);
     setShowLogin(true);
@@ -23,7 +32,26 @@ export const Landing = () => {
     setShowLogin(false);
     setShowSignUpForm(false);
   };
-  const handleTestAccountClick = () => {};
+  const handleTestAccountClick = () => {
+    const login = async () => {
+      try {
+        handleSetIsLoading(true);
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          testAccount.email,
+          testAccount.password
+        );
+        const user = userCredential.user;
+        localStorage.setItem("currentUser", JSON.stringify(user)); // save a user in localStorage
+        setUser(user);
+        handleSetIsLoading(false);
+      } catch (err: any) {
+        alert(err.message);
+        handleSetIsLoading(false);
+      }
+    };
+    login();
+  };
   return (
     <>
       <Loading />
